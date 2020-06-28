26665f49f907ba8e1f46e8b0df1392f4fde1200e
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-06-27 22:42:36 -0500

    Add gitignore and Initial Xcode project

---
+++ Bin2Dec/Bin2Dec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bin2Dec
+//
+//  Created by Akshay Ramesh on 6/27/20.
+//  Copyright © 2020 Akshay Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
